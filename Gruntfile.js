988ed137158eb9e74b52d7e357a522a09e20f708
ngot <zhuanghengfei@gmail.com>
2013-11-17 21:39:58 +0800

    push

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function (grunt) {
+  'use strict';
+  grunt.initConfig({
+    meta: {},
+    githubPages: {
+      target: {
+        options: {
+          // The default commit message for the gh-pages branch
+          commitMessage: 'push' ,
+          pushBranch : 'master'
+        },
+        // The folder where your gh-pages repo is
+        src: 'public'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-github-pages');
+
+  grunt.registerTask('deploy', ['githubPages:target']);
+}

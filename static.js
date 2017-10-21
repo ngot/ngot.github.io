ccfbaa805a075152f0b1350bf10f7790c0b4e41d
ngot <zhuanghengfei@gmail.com>
2017-10-21 19:19:14 +0800

    add tpl

---
+++ static.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const path = require('path');
+
+const port = 8000;
+const staticFileHandler = http.fileHandler(path.join(__dirname, 'docs'));
+
+const server = new http.Server(port, staticFileHandler);
+console.log(`preview server running on http://127.0.0.1:${port}`);
+server.run();

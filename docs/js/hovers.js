825cfd205289e7ab74223e4c98a2bc4e527e008e
Joanna Wu <joanna_wu@live.com>
2017-10-22 00:12:18 +1100

    remove async js

--- docs/js/hovers.js
+++ docs/js/hovers.js
@@ -1,15 +1,4 @@
-/**
- * hovers.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
 (function() {
-  
   function init() {
     var speed = 250,
       easing = mina.easeinout;
@@ -18,7 +7,7 @@
       var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
         pathConfig = {
           from : path.attr( 'd' ),
-          to : el.getAttribute( 'data-path-to' )
+          to : el.getAttribute( 'data-path-hover' )
         };
 
       el.addEventListener( 'mouseenter', function() {
